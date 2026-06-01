1de60d664b926e20f3227b16f8e04011c3a2b6d3
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-05-31 22:40:17 -0400

    Update package name to be org.appfair.app.netskip.ui

--- Android/app/src/main/kotlin/Main.kt
+++ Android/app/src/main/kotlin/Main.kt
@@ -32,7 +32,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 
-internal val logger: SkipLogger = SkipLogger(subsystem = "net.skip.App", category = "NetSkip")
+internal val logger: SkipLogger = SkipLogger(subsystem = "org.appfair.app.netskip", category = "NetSkip")
 
 /// AndroidAppMain is the `android.app.Application` entry point, and must match `application android:name` in the AndroidMainfest.xml file.
 open class AndroidAppMain: Application {
